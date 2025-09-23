7bfc9375a2a05bfdc526686d6e00c5e4a6accfc2
Sahil <sharmasahil0118@gmail.com>
2025-09-23 19:11:52 +0530

    Done

--- db/connectDb.js
+++ db/connectDb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   if (isConnected) return; // Prevent multiple connections
 
   try {
-    const conn = await mongoose.connect(process.env.Mongo_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     isConnected = true;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
   name: { type: String },
-  profilepic: { type: String, required: true },
-  coverpic: { type: String, required: true },
+  profilepic: { type: String },
+  coverpic: { type: String },
   razorpayid: { type: String },
   razorpaysecret: { type: String },
 });
